2589264ea4c70fa7aa83526fdec9bbea27b86901
Or <Or6699@gmail.com>
2020-11-12 17:19:06 +0200

    add the queenNode.py file.

--- QueenNode.py
+++ QueenNode.py
@@ -6,16 +6,15 @@ Created on Sat Jan 25 17:52:48 2020
 """
 """
 A class for defining an queen at n-queen solving problem.
-The c'tor receives the following arguments:
-    queen_column: every node have a queen name like queen 0 mean queen the place at column 0 at the board.
-    Dist: dictionary the saving all the pheramones that diposit at all "edges" values between this queen ant next queen at the board.
+The c'tor receive queen_column: every node have a queen name like queen 0 mean queen the place at column 0 at the board.
+and set Dist: dictionary the saving all the pheromones that deposit at all "edges" values between this queen ant next
+queen at the board.   
 """
+
+
 class QueenNode:
-    
-    # Class Attribute
-    queen_column = 0 
-    Dist = {}
-    
-     # Initializer / Instance Attributes
-    def __init__(self, y):
-        self.queen_column = y
+
+    # Initializer / Instance Attributes
+    def __init__(self, queen_column):
+        self.queen_column = queen_column
+        self.Dist = {}
